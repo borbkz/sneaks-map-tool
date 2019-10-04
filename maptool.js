f19323b98bd0e6f1b93242d48cade851285f5bbb
borb <55270215+borbkz@users.noreply.github.com>
2019-10-03 21:09:50 -0700

    s

--- maptool.js
+++ maptool.js
@@ -3,22 +3,23 @@ javascript:(function () {
     let playerIDIndex = parts.indexOf("players") + 1;
     let playerID = parts[playerIDIndex];
     let recordsURL = location.protocol + "//" + location.hostname + "/kzstats/api/player/" + playerID + '/records/';
+    $('table-container').remove();
+    $('.handsontable').remove();
+    $('#tooltip').remove();
+    $('table').remove();
     $('head').append('<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/handsontable/dist/handsontable.full.min.js">');
     $('head').append('<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/handsontable/dist/handsontable.full.min.css">');
-
-    $('table').hide();
     $('.pagination').hide();
     $('.recordstext').hide();
     $('.col-md-12').append('<div class="table-container" style="float:left !important; overflow: hidden; height: 800px; width: 800px; ">' +
         '<div style="float:left; width: 100%; color: white; text-align:left; font-size:1.2em">' +
-        '<span>click to sort, ctrl/cmd+click to multi-sort, type ":" in the time filter for finished maps and "/" for unfinished</span></div>' +
+        '<span id="tooltip">click to sort, ctrl/cmd+click to multi-sort, type ":" in the time filter for finished maps and "/" for unfinished</span></div>' +
         '<div class="handsontable col-md-12" style="margin:1" id="my-table">TABLE</div></div>');
     $tableContainer = $('#my-table')[0];
     function getTimeFromSeconds(seconds) {
         var hours = Math.floor(seconds / 3600);
         if (hours < 10){
             hours = "0" + hours;
-
         }
         seconds -= hours * 3600;
         var min = Math.floor(seconds / 60);
@@ -34,11 +35,11 @@ javascript:(function () {
     }
     $.getJSON(recordsURL, function (data) {
         let records = [];
-        let headers = ["Map", "TP Time", "TP's", "Pro Time"];
+        let headers = ["Map", "Pro Time","TP Time", "TPs"];
         $.each(data, function (i, record) {
             let emptytime = x => x === "-1" ? "n/a" : getTimeFromSeconds(x);
             let emptytp = x => x === "-1" ? "n/a" : x;
-            records.push([record["mapname"], emptytime(record["runtime"]), emptytp(record["teleports"]), emptytime(record["runtimepro"])]);
+            records.push([record["mapname"], emptytime(record["runtimepro"]),emptytime(record["runtime"]), emptytp(record["teleports"])]);
         });
         var debounceFn = Handsontable.helper.debounce(function (colIndex, event) {
             var filtersPlugin = mytable.getPlugin('filters');
@@ -63,7 +64,7 @@ javascript:(function () {
             if (typeof col !== 'number') {
                 return col;
             }
-            if (col >= 0 && TH.childElementCount < 2) {
+            if (col >= 0 && col!=headers.indexOf("TPs") && TH.childElementCount < 2) {
                 TH.appendChild(getInitializedElements(col));
             }
         };
@@ -73,11 +74,14 @@ javascript:(function () {
                 this.deselectCell();
             }
         };
+        mycolumns=Array(headers.length).fill({});
+        mycolumns[headers.indexOf("TPs")]={className:"htCenter"};
         var mytable = new Handsontable($tableContainer, {
             data: records,
             colHeaders: headers,
             multiColumnSorting: true,
             className: 'type-filter',
+            columns:mycolumns,
             filters: true,
             colWidths: 200,
             afterGetColHeader: addInput,
