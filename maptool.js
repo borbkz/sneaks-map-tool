9d9f78ea41535a0892a0bd971a81861bd92731e4
borb <55270215+borbkz@users.noreply.github.com>
2019-10-03 22:58:03 -0700

    added unfinished

--- maptool.js
+++ maptool.js
@@ -1,9 +1,10 @@
-javascript:(function () {
+javascript: (function () {
     let parts = window.location.href.split("/");
     let playerIDIndex = parts.indexOf("players") + 1;
     let playerID = parts[playerIDIndex];
     let recordsURL = location.protocol + "//" + location.hostname + "/kzstats/api/player/" + playerID + '/records/';
     const MAPS_URL = 'https://kztimerglobal.com/api/v1.0/maps?is_validated=true&limit=1000';
+    const SNEAK_MAPS_URL = 'https://snksrv.com/kzstats/api/map/';
     $('table-container').remove();
     $('.handsontable').remove();
     $('#tooltip').remove();
@@ -19,89 +20,99 @@ javascript:(function () {
     $tableContainer = $('#my-table')[0];
     function getTimeFromSeconds(seconds) {
         var hours = Math.floor(seconds / 3600);
-        if (hours < 10){
+        if (hours < 10) {
             hours = "0" + hours;
         }
         seconds -= hours * 3600;
         var min = Math.floor(seconds / 60);
-        if (min < 10){
+        if (min < 10) {
             min = "0" + min;
         }
         seconds -= min * 60;
         seconds = seconds.toFixed(2);
-        if (seconds < 10){
+        if (seconds < 10) {
             seconds = "0" + seconds
         }
         return hours + ":" + min + ":" + seconds;
     }
-    $.getJSON(recordsURL, function (data) {
-        let records = [];
-        let headers = ["Map", "Tier", "Pro Time","TP Time", "TPs"];
-        $.each(data, function (i, record) {
-            let emptytime = x => x === "-1" ? "n/a" : getTimeFromSeconds(x);
-            let emptytp = x => x === "-1" ? "n/a" : x;
-            records.push([record["mapname"], "", emptytime(record["runtimepro"]),emptytime(record["runtime"]), emptytp(record["teleports"])]);
+    $.getJSON(SNEAK_MAPS_URL, function (data) {
+        let headers = ["Map", "Tier", "Pro Time", "TP Time", "TPs"];
+        let sneakmaps = {};
+
+        $.each(data, function (i, map) {
+            sneakmaps[map["mapname"]] = Array(headers.length).fill("N/A");
+            sneakmaps[map["mapname"]][headers.indexOf("Map")] = map["mapname"];
         });
-        var debounceFn = Handsontable.helper.debounce(function (colIndex, event) {
-            var filtersPlugin = mytable.getPlugin('filters');
-            filtersPlugin.removeConditions(colIndex);
-            filtersPlugin.addCondition(colIndex, 'contains', [event.realTarget.value]);
-            filtersPlugin.filter();
-        }, 200);
-        var addEventListeners = function (input, colIndex) {
-            input.addEventListener('keydown', function (event) {
-                debounceFn(colIndex, event);
+        $.getJSON(recordsURL, function (data) {
+            let records = Object.values(sneakmaps);
+            $.each(data, function (i, record) {
+                let emptytime = x => x === "-1" ? "N/A" : getTimeFromSeconds(x);
+                let emptytp = x => x === "-1" ? "N/A" : x;
+                sneakmaps[record["mapname"]] = [record["mapname"], "", emptytime(record["runtimepro"]), emptytime(record["runtime"]), emptytp(record["teleports"])];
             });
-        };
-        var getInitializedElements = function (colIndex) {
-            var div = document.createElement('div');
-            var input = document.createElement('input');
-            div.className = 'filterHeader';
-            addEventListeners(input, colIndex);
-            div.appendChild(input);
-            return div;
-        };
-        var addInput = function (col, TH) {
-            if (typeof col !== 'number') {
-                return col;
-            }
-            if (col >= 0 && col!=headers.indexOf("TPs") && TH.childElementCount < 2) {
-                TH.appendChild(getInitializedElements(col));
-            }
-        };
-        var doNotSelectColumn = function (event, coords) {
-            if (coords.row === -1 && event.realTarget.nodeName === 'INPUT') {
-                event.stopImmediatePropagation();
-                this.deselectCell();
-            }
-        };
-        mycolumns=Array(headers.length).fill({});
-        mycolumns[headers.indexOf("TPs")]={className:"htCenter"};
-         mycolumns[headers.indexOf("Tier")]={className:"htCenter"};
-        var mytable = new Handsontable($tableContainer, {
-            data: records,
-            colHeaders: headers,
-            multiColumnSorting: true,
-            className: 'type-filter',
-            columns:mycolumns,
-            filters: true,
-            colWidths: 200,
-            afterGetColHeader: addInput,
-            beforeOnCellMouseDown: doNotSelectColumn,
-            licenseKey: 'non-commercial-and-evaluation'
+            var debounceFn = Handsontable.helper.debounce(function (colIndex, event) {
+                var filtersPlugin = mytable.getPlugin('filters');
+                filtersPlugin.removeConditions(colIndex);
+                filtersPlugin.addCondition(colIndex, 'contains', [event.realTarget.value]);
+                filtersPlugin.filter();
+            }, 200);
+            var addEventListeners = function (input, colIndex) {
+                input.addEventListener('keydown', function (event) {
+                    debounceFn(colIndex, event);
+                });
+            };
+            var getInitializedElements = function (colIndex) {
+                var div = document.createElement('div');
+                var input = document.createElement('input');
+                div.className = 'filterHeader';
+                addEventListeners(input, colIndex);
+                div.appendChild(input);
+                return div;
+            };
+            var addInput = function (col, TH) {
+                if (typeof col !== 'number') {
+                    return col;
+                }
+                if (col >= 0 && col != headers.indexOf("TPs") && TH.childElementCount < 2) {
+                    TH.appendChild(getInitializedElements(col));
+                }
+            };
+            var doNotSelectColumn = function (event, coords) {
+                if (coords.row === -1 && event.realTarget.nodeName === 'INPUT') {
+                    event.stopImmediatePropagation();
+                    this.deselectCell();
+                }
+            };
+            mycolumns = Array(headers.length).fill({});
+            mycolumns[headers.indexOf("TPs")] = { className: "htCenter" };
+            mycolumns[headers.indexOf("Tier")] = { className: "htCenter" };
+            mycolumns[headers.indexOf("Pro Time")] = { className: "htCenter" };
+            mycolumns[headers.indexOf("TP Time")] = { className: "htCenter" };
+            var mytable = new Handsontable($tableContainer, {
+                data: records,
+                colHeaders: headers,
+                multiColumnSorting: true,
+                className: 'type-filter',
+                columns: mycolumns,
+                filters: true,
+                colWidths: 200,
+                afterGetColHeader: addInput,
+                beforeOnCellMouseDown: doNotSelectColumn,
+                licenseKey: 'non-commercial-and-evaluation'
+            });
+            $.getJSON(MAPS_URL, function (data) {
+                let mapDict = {};
+                $.each(data, function (i, map) {
+                    mapDict[map["name"]] = map["difficulty"];
+                });
+                for (let i = 0; i < records.length; i++) {
+                    let curmap = records[i][0];
+                    let tier = mapDict[curmap];
+                    records[i][headers.indexOf("Tier")] = tier;
+                }
+                mytable.updateSettings({
+                });
+            });
         });
-         $.getJSON(MAPS_URL, function (data) {
-             let mapDict = {};
-             $.each(data, function(i, map){
-                mapDict[map["name"]] = map["difficulty"];
-             });
-             for(let i = 0; i < records.length; i++){
-                 let curmap = records[i][0];
-                 let tier = mapDict[curmap];
-                records[i][headers.indexOf("Tier")] = tier;
-             }
-             mytable.updateSettings({
-             });
-         });
-    })
+    });
 })();
